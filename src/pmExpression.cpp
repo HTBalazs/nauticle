55631bdfd3ec87f328af86d402b391d957ea0d5f
BalazsToth <t.balazs1986@gmail.com>
2016-11-19 13:51:29 +0100

    LGPL license added
    Former-commit-id: 9909b7b4241a2590223ad5148e27cb9dc64b16d9

--- src/pmExpression.cpp
+++ src/pmExpression.cpp
@@ -1,13 +1,23 @@
-/////////////////////////////////////////////////////////////////////////////
-// Name:        pmExpression.cpp
-// Purpose:     pmExpression class.
-// Author:      Balázs Tóth
-// Modified by:
-// Created:     10/07/2016
-// Copyright:   (c) LEMPS-project
-// Licence:     GPL
-/////////////////////////////////////////////////////////////////////////////
+/*
+    Copyright 2016 Balazs Toth
+    This file is part of LEMPS.
 
+    LEMPS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LEMPS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with LEMPS.  If not, see <http://www.gnu.org/licenses/>.
+
+    For more information please visit: https://BalazsToth@bitbucket.org/BalazsToth/lemps
+*/
+    
 #include "pmExpression.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////
