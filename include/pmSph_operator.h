d3f3a8b391cca60c034cd7da3de94d5953f2b6f4
balazs <t.balazs1986@gmail.com>
2017-03-21 13:23:22 +0100

    Rethinking SPH operator.
    Former-commit-id: 0f73c08e233fdc77a6ef9bee9c549568af801a7f

--- include/pmSph_operator.h
+++ include/pmSph_operator.h
@@ -1,5 +1,5 @@
 /*
-    Copyright 2016 Balazs Toth
+    Copyright 2017 Balazs Toth
     This file is part of LEMPS.
 
     LEMPS is free software: you can redistribute it and/or modify
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with LEMPS.  If not, see <http://www.gnu.org/licenses/>.
 
-    For more information please visit: https://BalazsToth@bitbucket.org/BalazsToth/lemps
+    For more information please visit: https://bitbucket.org/lempsproject/
 */
     
 #ifndef _SPH_OPERATOR_H_
@@ -30,14 +30,14 @@
 
 enum OPERATOR_TYPE { XSAMPLE, SAMPLE, GRADIENT, DIVERGENCE, LAPLACE };
 
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-class pmSph_operator : public pmSph<NUM_OP> {
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+class pmSph_operator : public pmSph<NOPS> {
 	std::string op_name;
 private:
 	std::shared_ptr<pmExpression> clone_impl() const override;
 public:
 	pmSph_operator() {}
-	pmSph_operator(std::array<std::shared_ptr<pmExpression>,NUM_OP> op);
+	pmSph_operator(std::array<std::shared_ptr<pmExpression>,NOPS> op);
 	pmSph_operator(pmSph_operator const& other);
 	pmSph_operator(pmSph_operator&& other);
 	pmSph_operator& operator=(pmSph_operator const& other);
@@ -50,20 +50,20 @@ public:
 	virtual void write_to_string(std::ostream& os) const override;
 };
 
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-void pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::write_to_string(std::ostream& os) const {
-	os<<op_name<<"("<<this->operand[0]<<","<< this->operand[1]<<","<< this->operand[2]<<","<<this->operand[3]<<")";
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+void pmSph_operator<OP_TYPE,VAR,K,NOPS>::write_to_string(std::ostream& os) const {
+	os<<op_name<<"("<<this->operand[0]<<","<<this->operand[1]<<","<<this->operand[2]<<","<<this->operand[3]<<")";
 }
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-std::ostream& operator<<(std::ostream& os, pmSph_operator<OP_TYPE, VAR, K, NUM_OP> const* obj) {
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+std::ostream& operator<<(std::ostream& os, pmSph_operator<OP_TYPE, VAR, K, NOPS> const* obj) {
 	obj->write_to_string(os);
 	return os;
 }
 /////////////////////////////////////////////////////////////////////////////////////////
 /// Constructor.
 /////////////////////////////////////////////////////////////////////////////////////////
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::pmSph_operator(std::array<std::shared_ptr<pmExpression>,NUM_OP> op) {
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+pmSph_operator<OP_TYPE,VAR,K,NOPS>::pmSph_operator(std::array<std::shared_ptr<pmExpression>,NOPS> op) {
 	this->operand = std::move(op);
 	size_t type = (int)this->operand[3]->evaluate(0)[0];
 	this->kernel = std::make_shared<pmKernel>();
@@ -81,8 +81,8 @@ pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::pmSph_operator(std::array<std::shared_ptr<
 /////////////////////////////////////////////////////////////////////////////////////////
 /// Copy constructor.
 /////////////////////////////////////////////////////////////////////////////////////////
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::pmSph_operator(pmSph_operator const& other) {
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+pmSph_operator<OP_TYPE,VAR,K,NOPS>::pmSph_operator(pmSph_operator const& other) {
 	this->assigned = false;
 	this->kernel = std::shared_ptr<pmKernel>(other.kernel);
 	for(int i=0; i<this->operand.size(); i++) {
@@ -93,8 +93,8 @@ pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::pmSph_operator(pmSph_operator const& other
 /////////////////////////////////////////////////////////////////////////////////////////
 /// Move constructor.
 /////////////////////////////////////////////////////////////////////////////////////////
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::pmSph_operator(pmSph_operator&& other) {
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+pmSph_operator<OP_TYPE,VAR,K,NOPS>::pmSph_operator(pmSph_operator&& other) {
 	this->psys = std::move(other.psys);
 	this->kernel = std::move(other.kernel);
 	this->assigned = std::move(other.assigned);
@@ -104,8 +104,8 @@ pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::pmSph_operator(pmSph_operator&& other) {
 /////////////////////////////////////////////////////////////////////////////////////////
 /// Copy assignment operator.
 /////////////////////////////////////////////////////////////////////////////////////////
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-pmSph_operator<OP_TYPE,VAR,K,NUM_OP>& pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::operator=(pmSph_operator const& other) {
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+pmSph_operator<OP_TYPE,VAR,K,NOPS>& pmSph_operator<OP_TYPE,VAR,K,NOPS>::operator=(pmSph_operator const& other) {
 	if(this!=&other) {
 		this->assigned = false;
 		this->kernel = std::shared_ptr<pmKernel>(other.kernel);
@@ -119,8 +119,8 @@ pmSph_operator<OP_TYPE,VAR,K,NUM_OP>& pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::oper
 /////////////////////////////////////////////////////////////////////////////////////////
 /// Move assignment operator.
 /////////////////////////////////////////////////////////////////////////////////////////
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-pmSph_operator<OP_TYPE,VAR,K,NUM_OP>& pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::operator=(pmSph_operator&& other) {
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+pmSph_operator<OP_TYPE,VAR,K,NOPS>& pmSph_operator<OP_TYPE,VAR,K,NOPS>::operator=(pmSph_operator&& other) {
 	if(this!=&other) {
 		this->psys = std::move(other.psys);
 		this->kernel = std::move(other.kernel);
@@ -133,55 +133,58 @@ pmSph_operator<OP_TYPE,VAR,K,NUM_OP>& pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::oper
 /////////////////////////////////////////////////////////////////////////////////////////
 /// Clone implementation.
 /////////////////////////////////////////////////////////////////////////////////////////
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-std::shared_ptr<pmExpression> pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::clone_impl() const {
-	return std::make_shared<pmSph_operator<OP_TYPE,VAR,K,NUM_OP>>(*this);
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+std::shared_ptr<pmExpression> pmSph_operator<OP_TYPE,VAR,K,NOPS>::clone_impl() const {
+	return std::make_shared<pmSph_operator<OP_TYPE,VAR,K,NOPS>>(*this);
 }
 /////////////////////////////////////////////////////////////////////////////////////////
 /// Returns the copy of the object.
 /////////////////////////////////////////////////////////////////////////////////////////
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-std::shared_ptr<pmSph_operator<OP_TYPE,VAR,K,NUM_OP>> pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::clone() const {
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+std::shared_ptr<pmSph_operator<OP_TYPE,VAR,K,NOPS>> pmSph_operator<OP_TYPE,VAR,K,NOPS>::clone() const {
 	return std::static_pointer_cast<pmSph_operator, pmExpression>(clone_impl());
 }
 /////////////////////////////////////////////////////////////////////////////////////////
 /// Prints SPH operator content.
 /////////////////////////////////////////////////////////////////////////////////////////
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-void pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::print() const {
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+void pmSph_operator<OP_TYPE,VAR,K,NOPS>::print() const {
 	pLogger::logf<COLOR>(op_name.c_str());
 	this->print_operands();
 }
 /////////////////////////////////////////////////////////////////////////////////////////
 /// Evaluates the operator for the ith node.
 /////////////////////////////////////////////////////////////////////////////////////////
-template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NUM_OP>
-pmTensor pmSph_operator<OP_TYPE,VAR,K,NUM_OP>::evaluate(int const& i, Eval_type eval_type/*=current*/) const {
+template <OPERATOR_TYPE OP_TYPE, size_t VAR, size_t K, size_t NOPS>
+pmTensor pmSph_operator<OP_TYPE,VAR,K,NOPS>::evaluate(int const& i, Eval_type eval_type/*=current*/) const {
 	if(!this->assigned) { pLogger::error_msgf("\"%s\" is not assigned to any particle system.\n", op_name.c_str()); }
-	size_t idx = NUM_OP==5?1:0;
-	pmTensor b_i{1,1,0};
-	if(NUM_OP==5) {
-		b_i = this->operand[0]->evaluate(i,eval_type);
+	size_t sh = 0;
+	pmTensor B_i{1,1,1};
+	if(NOPS==5) {
+		B_i = this->operand[0]->evaluate(i, eval_type);
+		sh++;
 	}
-	pmTensor A_i = this->operand[0+idx]->evaluate(i,eval_type);
-	float m_i = this->operand[1+idx]->evaluate(i,eval_type)[0];
-	float rho_i = this->operand[2+idx]->evaluate(i,eval_type)[0];
-	auto contribute = [&](pmTensor const& rel_pos, int const& i, int const& j, float const& cell_size, pmTensor const& guide)->pmTensor{
+	pmTensor A_i = this->operand[0+sh]->evaluate(i,eval_type);
+	float m_i = this->operand[1+sh]->evaluate(i,eval_type)[0];
+	float rho_i = this->operand[2+sh]->evaluate(i,eval_type)[0];
+	auto contribute = [&B_i, &sh, &A_i, &m_i, &rho_i, &eval_type, this](pmTensor const& rel_pos, int const& i, int const& j, float const& cell_size, pmTensor const& guide)->pmTensor{
 		pmTensor contribution;
-		if(i!=j || OP_TYPE==SAMPLE) {
-			float d_ji = rel_pos.norm();
-			if(d_ji < cell_size && (d_ji > 1e-6f || OP_TYPE==SAMPLE)) {
-				pmTensor b_j{1,1,0};
-				size_t idx = NUM_OP==5?1:0;
-				if(NUM_OP==5) {
-					b_j = this->operand[0]->evaluate(j,eval_type);
+		float d_ji = rel_pos.norm();
+		if(d_ji > 1e-6f || OP_TYPE==SAMPLE) {
+			if(d_ji < cell_size) {
+				pmTensor B_j{1,1,1};
+				if(NOPS==5) {
+					B_j = this->operand[0]->evaluate(j, eval_type);
 				}
-				pmTensor A_j = this->operand[0+idx]->evaluate(j,eval_type);
-				float m_j = this->operand[1+idx]->evaluate(j,eval_type)[0];
-				float rho_j = this->operand[2+idx]->evaluate(j,eval_type)[0];
+				pmTensor A_j = this->operand[0+sh]->evaluate(j,eval_type).reflect(guide);
+				float m_j = this->operand[1+sh]->evaluate(j,eval_type)[0];
+				float rho_j = this->operand[2+sh]->evaluate(j,eval_type)[0];
 				float W_ij = this->kernel->evaluate(d_ji, cell_size);
-				pmTensor tmp = this->process(A_i, A_j, rho_i, rho_j, m_i, m_j, rel_pos, d_ji, W_ij);
-				contribution += (NUM_OP==4 ? tmp : tmp*(b_i+b_j)/2.0);
+				pmTensor B_ij{1,1,1};
+				if(NOPS==5) {
+					B_ij = (B_i+B_j)/2.0f;
+				}
+				contribution += B_ij*this->process(A_i, A_j, rho_i, rho_j, m_i, m_j, rel_pos, d_ji, W_ij);
 			}
 		}
 		return contribution;
@@ -236,7 +239,8 @@ inline pmTensor pmSph_operator<DIVERGENCE,1,0,4>::process(pmTensor const& A_i, p
 template<>
 inline pmTensor pmSph_operator<LAPLACE,0,0,4>::process(pmTensor const& A_i, pmTensor const& A_j, float const& rho_i, float const& rho_j, float const& m_i, float const& m_j, pmTensor const& r_ji, float const& d_ji, float const& W_ij) const {
 	pmTensor e_ij = -r_ji/d_ji;
-	return (2.0f*m_j/rho_j*e_ij.transpose()/d_ji*(W_ij*e_ij))*(A_i-A_j);
+	// return 2.0f*e_ij.transpose()/d_ji*(A_i-A_j)*m_j/rho_j*W_ij*e_ij;
+	return 2.0f*e_ij.transpose()/d_ji*W_ij*e_ij*(A_i-A_j)*m_j/rho_j;
 }
 /////////////////////////////////////////////////////////////////////////////////////////
 /// Evaluates the operator.
@@ -244,7 +248,7 @@ inline pmTensor pmSph_operator<LAPLACE,0,0,4>::process(pmTensor const& A_i, pmTe
 template<>
 inline pmTensor pmSph_operator<LAPLACE,1,0,5>::process(pmTensor const& A_i, pmTensor const& A_j, float const& rho_i, float const& rho_j, float const& m_i, float const& m_j, pmTensor const& r_ji, float const& d_ji, float const& W_ij) const {
 	pmTensor e_ij = -r_ji/d_ji;
-	return (2.0f*m_j/rho_j*e_ij.transpose()/d_ji*(W_ij*e_ij))*(A_i-A_j);
+	return 2.0f*e_ij.transpose()/d_ji*(A_i-A_j)*m_j/rho_j*W_ij*e_ij;
 }
 /////////////////////////////////////////////////////////////////////////////////////////
 /// Evaluates the operator.
