0f51be05d2ff596a3989259623668bebb7c9207e
balazs <t.balazs1986@gmail.com>
2017-04-13 02:20:30 +0200

    Setting depth of variable's storage.
    Former-commit-id: 82f3aa6103f1e4078adc93d2ffdaeaecbd7b7f99

--- src/pmVariable.cpp
+++ src/pmVariable.cpp
@@ -29,6 +29,7 @@ pmVariable::pmVariable(std::string const& n, pmTensor const& v/*=pmTensor{0}*/) 
 }
 
 void pmVariable::set_storage_depth(size_t const& d) {
+    depth = d;
     value.resize(d);
 }
 
