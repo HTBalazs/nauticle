dd0c4ead6c80fc57d53ee6211e7d7fcec63febe5
balazs <t.balazs1986@gmail.com>
2017-07-12 12:28:56 +0200

    Fix dvm operator name
    Former-commit-id: f781fe25364645a82d00eeb9cb92d70ea9361728

--- src/pmExpression_parser.cpp
+++ src/pmExpression_parser.cpp
@@ -353,7 +353,7 @@ std::shared_ptr<pmExpression> pmExpression_parser::build_expression_tree(std::ve
 				stack_extract(e, operands);
 				e.push(std::make_shared<pmSph_operator<LAPLACE,1,0,6>>(operands));
 			}
-			if(it=="vm") {
+			if(it=="dvm") {
 				std::array<std::shared_ptr<pmExpression>,3> operands;
 				stack_extract(e, operands);
 				e.push(std::make_shared<pmDvm_operator>(operands));
