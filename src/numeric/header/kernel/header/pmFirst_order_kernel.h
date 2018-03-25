e07011db55121cb7625fc8b683eb07a2a51572cd
balazs <t.balazs1986@gmail.com>
2018-03-26 01:54:06 +0200

    Create new classes for all kernel functions.
    Former-commit-id: 4fbeac72a681916e639c9bd2843df0ba75d0cc2c

---
+++ src/numeric/header/kernel/header/pmFirst_order_kernel.h
@@ -0,0 +1,83 @@
+/*
+    Copyright 2016-2018 Balazs Toth
+    This file is part of Nauticle.
+
+    Nauticle is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Nauticle is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Nauticle.  If not, see <http://www.gnu.org/licenses/>.
+
+    For more information please visit: https://bitbucket.org/nauticleproject/
+*/
+
+#ifndef _FIRST_ORDER_KERNEL_H_
+#define _FIRST_ORDER_KERNEL_H_
+
+#include "pmKernel.h"
+#include <cmath>
+#include "nauticle_constants.h"
+
+namespace nauticle {
+	/** This class contains the first order smoothing kernel implementations for 1, 2 and 3 dimensions.
+	*/
+    template<size_t dimension, bool derivative>
+    class pmFirst_order_kernel : public pmKernel<dimension,derivative> {
+        double coefficient(double const& influence_radius) const override;
+        double kernel_at(double const& q) const;
+    public:
+        pmFirst_order_kernel();
+        double evaluate(double const& r, double const& influence_radius) const override;
+    };
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    /// Constructor.
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template<size_t dimension, bool derivative>
+    pmFirst_order_kernel<dimension,derivative>::pmFirst_order_kernel() {
+        name = derivative?"d":""+"Wp111"+std::to_string(dimension)+"0";
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    /// Returns the normalization coefficient of the kernel function or its derivative.
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template<size_t dimension, bool derivative>
+    double pmFirst_order_kernel<dimension,derivative>::coefficient(double const& influence_radius) const {
+        switch(dimension) {
+            default:
+            case 1 : return derivative ? 1.0/influence_radius/influence_radius : 1.0/influence_radius;
+            case 2 : return derivative ? 3.0/NAUTICLE_PI/influence_radius/influence_radius/influence_radius : 3.0/NAUTICLE_PI/influence_radius/influence_radius;
+            case 3 : return derivative ? 3.0/NAUTICLE_PI/influence_radius/influence_radius/influence_radius/influence_radius : 3.0/NAUTICLE_PI/influence_radius/influence_radius/influence_radius;
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    /// Returns the value of the kernel function or its derivative at a given place q=r/h.
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template<size_t dimension, bool derivative>
+    double pmFirst_order_kernel<dimension,derivative>::kernel_at(double const& q) const {
+        if(!derivative) {
+            return 1.0-q;
+        } else {
+            return -1.0;
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    /// Returns the kernel value or its derivative at a given distance r and radius influence_radius.
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template<size_t dimension, bool derivative>
+    double pmFirst_order_kernel<dimension,derivative>::evaluate(double const& r, double const& influence_radius) const {
+        double q = r/influence_radius;
+        return coefficient(influence_radius)*kernel_at(q);
+    }
+}
+
+#endif // _FIRST_ORDER_KERNEL_H_
---
+++ src/numeric/header/kernel/header/pmThird_order_kernel.h
@@ -0,0 +1,86 @@
+/*
+    Copyright 2016-2018 Balazs Toth
+    This file is part of Nauticle.
+
+    Nauticle is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Nauticle is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Nauticle.  If not, see <http://www.gnu.org/licenses/>.
+
+    For more information please visit: https://bitbucket.org/nauticleproject/
+*/
+
+#ifndef _THIRD_ORDER_KERNEL_H_
+#define _THIRD_ORDER_KERNEL_H_
+
+#include "pmKernel.h"
+#include <cmath>
+#include "nauticle_constants.h"
+
+namespace nauticle {
+	/** This class contains the third order smoothing kernel implementations for 1, 2 and 3 dimensions.
+	*/
+    template<size_t dimension, bool derivative>
+    class pmThird_order_kernel : public pmKernel<dimension,derivative> {
+        double coefficient(double const& h) const override;
+        double kernel_at(double const& q) h;
+    public:
+        pmThird_order_kernel();
+        double evaluate(double const& r, double const& influence_radius) const override;
+    };
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    /// Constructor.
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template<size_t dimension, bool derivative>
+    pmThird_order_kernel<dimension,derivative>::pmThird_order_kernel() {
+        name = derivative?"d":""+"Wp322"+std::to_string(dimension)+"0";
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    /// Returns the normalization coefficient of the kernel function or its derivative.
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template<size_t dimension, bool derivative>
+    double pmThird_order_kernel<dimension,derivative>::coefficient(double const& h) const {
+        switch(dimension) {
+            default:
+            case 1 : return derivative ? 6.0/12.0/h/h : 2.0/3.0/h;
+            case 2 : return derivative ? 30.0/28.0/NAUTICLE_PI/h/h/h : 10.0/7.0/NAUTICLE_PI/h/h;
+            case 3 : return derivative ? 3.0/4.0/NAUTICLE_PI/h/h/h/h : 1.0/NAUTICLE_PI/h/h/h;
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    /// Returns the value of the kernel function or its derivative at a given place q=r/h.
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template<size_t dimension, bool derivative>
+    double pmThird_order_kernel<dimension,derivative>::kernel_at(double const& q) const {
+        if(!derivative) {
+            if(q<1) { return 1.0-3.0/2.0*q*q+3.0/4.0*q*q*q; }
+            else    { return 0.25*(2.0-q)*(2.0-q)*(2.0-q); }
+        } else {
+            if(q<1) { return (-4.0*q+3.0*q*q); }
+            else    { return -(2.0-q)*(2.0-q); }
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    /// Returns the kernel value or its derivative at a given distance r and radius influence_radius.
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template<size_t dimension, bool derivative>
+    double pmThird_order_kernel<dimension,derivative>::evaluate(double const& r, double const& influence_radius) const {
+        double h = influence_radius/2.0;
+        double q = r/h;
+        return coefficient(h)*kernel_at(q);
+    }
+}
+
+#endif // _THIRD_ORDER_KERNEL_H_
